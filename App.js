2c2e731e9ac01635b4ca4ed7ae262fcd984e2958
Narek06 <narektonoyan82@gmail.com>
2022-09-14 02:26:12 +0400

    location sucsessfully

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Alert, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as Location from 'expo-location';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function App() {
@@ -9,12 +9,12 @@ export default function App() {
   const [lat, setLat] = useState(0)
   const [long, setLong] = useState(0)
   const [color, setColor] = useState("aqua")
-  const fixLat = 40.5731
-  const fixlong = 44.8170
+  const fixLat = 40.3553
+  const fixlong = 45.1238
   const getLocation = async () => {
     try {
       await Location.requestForegroundPermissionsAsync()
-      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync()
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync()
       setLat(latitude)
       setLong(longitude)
       console.log(latitude, longitude);
@@ -23,7 +23,7 @@ export default function App() {
     }
     const transformLat = []
     const transformLong = []
-    let x = "" + lat 
+    let x = "" + lat
     let y = "" + long
     for (let i = 0; i < 7; i++) {
       transformLat.push(x[i])
@@ -31,27 +31,29 @@ export default function App() {
     for (let i = 0; i < 7; i++) {
       transformLong.push(y[i])
     }
-    if (fixLat == +transformLat.join("") || fixlong  == +transformLong.join("")) {
-      
+    if (fixLat == +transformLat.join("") || fixlong == +transformLong.join("")) {
+
       setColor("green")
     } else {
       setColor("red")
     }
     console.log(`fix lat ${fixLat} lat ${+transformLat.join("")}`);
-  } 
+  }
 
+  useEffect(() => {
+    getLocation()
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>40.5731</Text>
-      <Text>44.8170</Text>
-      <TouchableOpacity 
-      style = {{backgroundColor: `${color}`, padding: 10}}
-      onPress = {() => getLocation()}
+      <Text>40.3553</Text>
+      <Text>45.1238</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: `${color}`, padding: 10 }}
+        onPress={() => getLocation()}
       >
         <Text>Click</Text>
       </TouchableOpacity>
-      <Text>{lat}</Text>
-      <Text>{long}</Text>
+
 
     </View>
   );
