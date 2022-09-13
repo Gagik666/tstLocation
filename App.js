114f640e93cffae494af41e80d24a3367cbe1764
Gagik666 <gagikvardanyan6666@gmail.com>
2022-09-13 22:55:37 +0400

    add project

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as Location from 'expo-location';
+import { useState } from 'react';
+
 
 export default function App() {
+
+  const [lat, setLat] = useState(0)
+  const [long, setLong] = useState(0)
+  const [color, setColor] = useState("aqua")
+  const fixLat = 40.5731434
+  const fixlong = 44.8170515
+  const getLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync()
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync()
+      setLat(latitude)
+      setLong(longitude)
+      console.log(latitude, longitude);
+    } catch (error) {
+      Alert.alert("error", "eror location")
+    }
+
+    if ((fixLat >= (lat - 0.0002) || fixLat <= (lat + 0.0002)) 
+    && (fixlong >= (long - 0.0002) || fixlong <= (long + 0.0002))) {
+      console.log("stacvec");
+      setColor("green")
+    } else {
+      setColor("red")
+    }
+    
+  } 
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>40.5731434</Text>
+      <Text>44.8170515</Text>
+      <TouchableOpacity 
+      style = {{backgroundColor: `${color}`, padding: 10}}
+      onPress = {() => getLocation()}
+      >
+        <Text>Click</Text>
+      </TouchableOpacity>
+      <Text>{lat}</Text>
+      <Text>{long}</Text>
+
     </View>
   );
 }
